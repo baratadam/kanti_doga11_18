085fdb7cadf7c0c376683701d019b1cb1c8ee684
Baráth Ádám <144095347+baratadam@users.noreply.github.com>
2024-11-18 11:23:40 +0100

    Add files via upload

--- doga.js
+++ doga.js
@@ -1,3 +1,4 @@
+//Dolgozat
 //Dna to Rna
 function DNAtoRNA(dna){
     return dna.replace(/T/g, 'U');
@@ -6,14 +7,24 @@ function DNAtoRNA(dna){
 function disemvowel(str){
     return str.replace(/[aeiou]/gi,"");
 }
-Fizz Buzz - Without ifs
+//Fizz Buzz - Without ifs
 const fizzBuzz = n => {
-    const result = [
-      n % 3 == 0 && n % 5 == 0 && "FizzBuzz",
-      n % 3 == 0 && "Fizz",
-      n % 5 == 0 && "Buzz",
-      n
-      ].filter(Boolean)[0];
-    return result;
-  };
-
+    return
+      (n % 15 == 0 && "FizzBuzz") ||
+      (n % 3 == 0 && "Fizz") ||
+      (n % 5 == 0 && "Buzz") ||
+       n 
+    };
+//Correct the date-string
+function dateCorrect(dateString) {
+    if (!dateString) {
+      return dateString
+    }
+    const match = dateString.match(/^(\d{2})\.(\d{2})\.(\d{4})$/)
+    if (match) {
+      const date = new Date(match[3], match[2] - 1, match[1], 12)
+      return date.toJSON().replace(/^(\d+)-(\d+)-(\d+)T.+/, '$3.$2.$1')
+    } else {
+      return null
+    }
+  }
