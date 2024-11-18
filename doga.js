6dec127cc4daef6a31ef80a57cab5b226551959e
Baráth Ádám <144095347+baratadam@users.noreply.github.com>
2024-11-18 11:29:15 +0100

    Add files via upload

--- doga.js
+++ doga.js
@@ -9,10 +9,9 @@ function disemvowel(str){
 }
 //Fizz Buzz - Without ifs
 const fizzBuzz = n => {
-    return
-      (n % 15 == 0 && "FizzBuzz") ||
-      (n % 3 == 0 && "Fizz") ||
-      (n % 5 == 0 && "Buzz") ||
+return  (n % 15 === 0 && "FizzBuzz") ||
+        (n % 3 === 0 && "Fizz") ||
+        (n % 5 == 0 && "Buzz") ||
        n 
     };
 //Correct the date-string
